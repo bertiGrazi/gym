02d73b5be9a001dae6321e688a58454d69c59cbc
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-12-19 10:52:32 -0300

    customizing the status bar

--- gym/App.tsx
+++ gym/App.tsx
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { Text, View } from 'react-native';
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Teste da Grazi!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#202024'}}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor='transparent'
+        translucent
+      />
+      {fontsLoaded ? <Text>Teste da Grazi!</Text> : <View /> } 
     </View>
   );
 }
